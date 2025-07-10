8b6e71d145393aebc06327aa71ae4cf79f8d479c
Aaron Greenberg <aaron@friendlycaptcha.com>
2025-07-10 15:02:08 +0200

    Fix configuration pulling in LWC

--- force-app/main/default/lwc/friendlyCaptchaWidget/friendlyCaptchaWidget.js
+++ force-app/main/default/lwc/friendlyCaptchaWidget/friendlyCaptchaWidget.js
@@ -1,7 +1,7 @@
 import { LightningElement, api, wire } from 'lwc';
 import { loadScript } from 'lightning/platformResourceLoader';
 import FriendlyCaptchaSDK from '@salesforce/resourceUrl/FriendlyCaptchaSDK';
-import loadConfig from '@salesforce/apex/SettingsController.get'
+import getConfig from '@salesforce/apex/SettingsController.get'
 
 export default class FriendlyCaptchaWidget extends LightningElement {
   @api sitekey;
@@ -10,33 +10,69 @@ export default class FriendlyCaptchaWidget extends LightningElement {
   @api language;
   @api theme;
 
-  @wire(loadConfig)
-  config;
-
+  configFetched = false;
+  htmlRendered = false;
+  remoteConfig = {};
   widget = null;
 
+  @wire(getConfig) fetchConfig({ data, error }) {
+    // On construction, these are both empty, exit early.
+    if (!data && !error) {
+      return;
+    }
+
+    this.configFetched = true;
+
+    if (data) {
+      this.remoteConfig = data;
+    } else if (error) {
+      console.warn('[FriendlyCaptchaWidget] Error fetching configuration:', error);
+    }
+
+    this.renderWidget();
+  };
+
   async renderedCallback() {
+    this.htmlRendered = true;
+
     if (!window.frcaptcha) {
       await loadScript(this, FriendlyCaptchaSDK);
     }
 
-    if (this.widget === null) {
-      const opts = {
-        element: this.refs.frcaptcha,
-        sitekey: this.sitekey || this.config?.data.sitekey,
-        apiEndpoint: this.apiEndpoint || this.config?.data.apiEndpoint || 'global',
-        startMode: this.startMode || this.config?.data.startMode || 'focus',
-        theme: this.theme || this.config?.data.theme || 'light',
-      }
+    this.renderWidget();
+  }
 
-      const lang = this.language || this.config?.data.language;
-      if (lang) {
-        opts.language = lang;
-      }
-
-      this.widget = frcaptcha.createWidget(opts);
-    } else {
-      this.widget.reset();
+  renderWidget() {
+    // Only load the widget once the HTML is available
+    // and the settings have been retrieved from the back-end.
+    if (!(this.htmlRendered && this.configFetched)) {
+      return;
     }
+
+    // Guard for the widget already existing.
+    if (this.widget !== null) {
+      return;
+    }
+
+    const sitekey = this.sitekey || this.remoteConfig.sitekey;
+    if (!sitekey) {
+      console.error('[FriendlyCaptchaWidget] Missing "sitekey" parameter')
+      return;
+    }
+
+    const opts = {
+      element: this.refs.frcaptcha,
+      sitekey: sitekey,
+      apiEndpoint: this.apiEndpoint || this.remoteConfig.apiEndpoint || 'global',
+      startMode: this.startMode || this.remoteConfig.startMode || 'focus',
+      theme: this.theme || this.remoteConfig.theme || 'light',
+    }
+
+    const language = this.language || this.remoteConfig.language;
+    if (language) {
+      opts.language = language;
+    }
+
+    this.widget = frcaptcha.createWidget(opts);
   }
 }
